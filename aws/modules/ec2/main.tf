da753d19eed307a3fd5438300791f9d64ec347fd
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-09-24 13:05:31 -0300

    name fix

--- aws/modules/ec2/main.tf
+++ aws/modules/ec2/main.tf
@@ -1,15 +1,5 @@
-module "sg" {
-  source = "terraform-aws-modules/security-group/aws"
-
-  name   = "prod-sg"
-  vpc_id = var.vpc_id
-
-  ingress_rules = ["ssh-tcp"]
-  egress_rules  = ["https-443-tcp", "http-80-tcp"]
-}
-
 resource "aws_iam_instance_profile" "main" {
-  name = "ec2-test-profile"
+  name = "EC2TestProfile"
   role = aws_iam_role.main.arn
 }
 
@@ -21,13 +11,15 @@ module "ec2_instance" {
   ami                         = "ami-08fdd91d87f63bb09"
   monitoring                  = true
   associate_public_ip_address = true
-  vpc_security_group_ids      = [module.sg.security_group_id]
+  vpc_security_group_ids      = [aws_security_group.main.id]
   subnet_id                   = var.subnet_id
   iam_instance_profile        = aws_iam_instance_profile.main.id
 }
 
+### IAM ###
+
 resource "aws_iam_role" "main" {
-  name = "prod-ec2-role"
+  name = "ProdEC2Role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -52,4 +44,53 @@ resource "aws_iam_role_policy_attachment" "ssm-managed-instance-core" {
 resource "aws_iam_role_policy_attachment" "cloudwatc-agent-server-policy" {
   role       = aws_iam_role.main.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+
+### Security Group ###
+
+resource "aws_security_group" "main" {
+  name        = "ec2-instance-sg"
+  description = "Allow application and management"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "sg-ec2-instance"
+  }
+}
+
+resource "aws_security_group_rule" "ingress_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
+}
+
+resource "aws_security_group_rule" "ingress_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
+}
+
+resource "aws_security_group_rule" "egress_http" {
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
+}
+
+resource "aws_security_group_rule" "egress_https" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
 }
