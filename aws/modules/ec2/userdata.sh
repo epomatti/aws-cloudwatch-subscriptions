dcb6b2d140ddaff7470a914a8d989f6f91ef0fc1
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-10-22 15:03:12 -0300

    cloudwatch agent

--- aws/modules/ec2/userdata.sh
+++ aws/modules/ec2/userdata.sh
@@ -7,9 +7,8 @@ apt update
 apt upgrade -y
 
 # CloudWatch Agent
-# https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/install-CloudWatch-Agent-commandline-fleet.html
-wget https://amazoncloudwatch-agent-region.s3.region.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
-sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+wget https://amazoncloudwatch-agent-us-east-2.s3.us-east-2.amazonaws.com/ubuntu/arm64/latest/amazon-cloudwatch-agent.deb
+dpkg -i -E ./amazon-cloudwatch-agent.deb
 
 
 reboot
